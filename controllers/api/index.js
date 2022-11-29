bf7faaca3010c53cbafdce4f9f88d9dd332f50e1
Grant Thorne <gthorne61@gmail.com>
2022-11-28 23:17:47 -0600

    bugging out

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,8 @@ const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
-
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
